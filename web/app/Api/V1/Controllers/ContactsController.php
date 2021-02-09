43b463c6e06ea7150bfa55b81656e6c1528b2a32
yuriy <bibrkacity@gmail.com>
2021-02-09 18:22:52 +0200

    Prevent dubles

--- web/app/Api/V1/Controllers/ContactsController.php
+++ web/app/Api/V1/Controllers/ContactsController.php
@@ -156,6 +156,9 @@ class ContactsController extends Controller
 
 private function save($userID,$json)
  {
+     try {
+
+
      $client = ClientBuilder::create()
          ->addConnection('default', env('NEO_DEFAULT_URL','http://neo4j:kanku@localhost:7474')) // Example for HTTP connection configuration (port is optional)
          ->addConnection('bolt', env('NEO_BOLT_URL','bolt://neo4j:kanku@localhost:7687')) // Example for BOLT connection configuration (port is optional)
@@ -165,8 +168,22 @@ private function save($userID,$json)
      //Look for a user id= $userID. If not found, create such a user.
      $query = "MERGE (person:User {  id:$userID })
 RETURN person";
-
      $client->run($query);
+
+     //get existing contacts
+     $query = "MATCH (person:User {  id:$userID })-[:LISTEN]->(Contact) RETURN collect(Contact) as contacts";
+     $result = $client->run($query);
+     $existing = [];
+     $records = $result->getRecords();
+     foreach ($records as $record) {
+         foreach($record->value('contacts') as $one)
+         {
+             $name = $one->value('name');
+             $value = $one->value('text');
+             $existing[] = ['name'=>$name, 'text'=>$value];
+         }
+     }
+
 
      foreach($json as $one)
      {
@@ -175,6 +192,20 @@ RETURN person";
          {
              $name = $key;
              $text = $value;
+
+             $exists = false;
+
+             foreach($existing as $one)
+             {
+                 if( ($one['name'] == $name) && ($one['text'] == $text))
+                 {
+                     $exists = true;
+                     break;
+                 }
+             }
+
+             if($exists)
+                 continue;
 
             $query = "MATCH (person:User) WHERE person.id = $userID
             CREATE (ct:Contact {name: \"$name\", text:\"".$text."\"}),
@@ -185,6 +216,11 @@ RETURN person";
      }
 
     return 'Ok';
+     }
+     catch(\Exception $e)
+     {
+         return $e->getMessage();
+     }
  }
 
 
