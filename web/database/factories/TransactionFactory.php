47db2036c1383709f55c3c799e7a7e0cd595ec13
gas <fireworld116@gmail.com>
2021-09-01 15:09:57 +0300

    wrote factory for the  transaction model

---
+++ web/database/factories/TransactionFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Transaction;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Transaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'user_id' => function() {
+                return \App\Models\User::all()->random()->id;
+            },
+            'user_plan' => $this->faker->randomElement(['basic', 'bronze', 'silver', 'gold']),
+            'reward' => rand(1, 20),
+            'currency' => $this->faker->currencyCode,
+            'operation_name' => $this->faker->unique()->word,
+        ];
+    }
+}
--- web/database/migrations/2021_09_01_112440_create_totals_table.php
+++ web/database/migrations/2021_09_01_112440_create_totals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTotalTable extends Migration
+class CreateTotalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateTotalTable extends Migration
      */
     public function up()
     {
-        $table_name = 'total';
+        $table_name = 'totals';
 
         Schema::create($table_name, function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')
+                ->primary();
 
             $table->integer('amount')
                 ->unsigned()
