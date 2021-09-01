79e7caa04eedeaf96a37c4ff3983e3268c9933d5
gas <fireworld116@gmail.com>
2021-09-01 11:33:17 +0300

    wrote model for the total migration

---
+++ web/app/Models/Total.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Total extends Model
+{
+    use HasFactory;
+    use UuidTrait;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username',
+        'email',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * @var array|string[]
+     */
+    public static array $rules = [
+        'amount' => 'integer',
+        'reward' => 'regex:/^\d*(\.\d{2})?$/',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
