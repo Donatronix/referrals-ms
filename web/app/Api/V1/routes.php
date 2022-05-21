5960e1fca0b97788376d5dce6e54b1bf3a0a1e01
Dhana <ihor.porokhnenko@gmail.com>
2022-05-21 18:15:40 +0300

    upgrade docker and fix

--- web/app/Api/V1/routes.php
+++ web/app/Api/V1/routes.php
@@ -5,13 +5,17 @@
  */
 $router->group([
     'prefix' => env('APP_API_VERSION', ''),
-    'namespace' => '\App\Api\V1\Controllers',
+    'namespace' => '\App\Api\V1\Controllers'
 ], function ($router) {
     /**
-     * Internal access
+     * PUBLIC ACCESS
+     */
+
+    /**
+     * PRIVATE ACCESS
      */
     $router->group([
-        'middleware' => 'checkUser',
+        'middleware' => 'checkUser'
     ], function ($router) {
         /**
          * Referrals
@@ -46,15 +50,15 @@ $router->group([
     });
 
     /**
-     * ADMIN PANEL
+     * ADMIN PANEL ACCESS
      */
     $router->group([
         'prefix' => 'admin',
         'namespace' => 'Admin',
         'middleware' => [
             'checkUser',
-            'checkAdmin',
-        ],
+            'checkAdmin'
+        ]
     ], function ($router) {
         /**
          * Referrals
