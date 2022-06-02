08c05c46394ffc1cb548b06d6c45b156f3712d56
Donald Blessing <donaldblessing9@gmail.com>
2022-06-02 20:46:37 +0100

    Added filter for user

--- web/app/Api/V1/Controllers/ReferralController.php
+++ web/app/Api/V1/Controllers/ReferralController.php
@@ -302,8 +302,11 @@ class ReferralController extends Controller
     public function getReferralTotals(Request $request)
     {
         try {
-
-            $total = Total::all()->sum('reward');
+            if ($request->has('user_id')) {
+                $total = Total::where('user_id', $request->user_id)->get()->sum('reward');
+            } else {
+                $total = Total::all()->sum('reward');
+            }
 
             // Return response
             return response()->jsonApi([
