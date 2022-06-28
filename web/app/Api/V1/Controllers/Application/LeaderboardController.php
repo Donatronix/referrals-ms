615290a791fff681778f3138d4054ca9da12e532
Donald Blessing <donaldblessing9@gmail.com>
2022-06-28 11:58:04 +0100

    Implemented table joins with pagination

--- web/app/Api/V1/Controllers/Application/LeaderboardController.php
+++ web/app/Api/V1/Controllers/Application/LeaderboardController.php
@@ -12,6 +12,7 @@ use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Throwable;
 
 class LeaderboardController extends Controller
@@ -201,7 +202,38 @@ class LeaderboardController extends Controller
         $user_id = Auth::user()->getAuthIdentifier();
 
         try {
-            $leaderboard = $this->getLeaderboard($request);
+            $filter = strtolower($request->filter);
+
+//            $leaderboard = $this->getLeaderboard($request);
+
+
+            $channels = $this->getFilterQuery(DB::table('referral_codes')
+                ->distinct('application_id')
+                ->select('application_id', 'user_id'), $filter);
+
+            $totalReward = $this->getFilterQuery(DB::table('totals')
+                ->select('user_id', 'twenty_four_hour_percentage', DB::raw('SUM(reward) as endOfYearCashPrize')), $filter)
+                ->groupBy('user_id', 'twenty_four_hour_percentage');
+
+
+            $leaderboard = DB::table('users')
+                ->whereNotNull('referrer_id')->distinct('referrer_id')
+                ->select('name',
+                    'country',
+                    DB::raw('COUNT(referrer_id) as referrals'),
+                    'totals.endOfYearCashPrize as endOfYearCashPrize',
+                    'totals.twenty_four_hour_percentage as twentyFourHourPercentage',
+                    'channels.application_id as channels'
+                )
+                ->joinSub($channels, 'channels', function ($join) {
+                    $join->on('users.referrer_id', '=', 'channels.user_id');
+                })
+                ->joinSub($totalReward, 'totals', function ($join) {
+                    $join->on('users.referrer_id', '=', 'totals.user_id');
+                })
+                ->groupBy('users.name', 'users.country', 'totals.endOfYearCashPrize', 'totals.twenty_four_hour_percentage', 'channels.application_id')
+                ->orderBy('endOfYearCashPrize', 'desc')
+                ->paginate(config('settings.pagination_limit'));
 
             return response()->jsonApi([
                 'type' => 'success',
